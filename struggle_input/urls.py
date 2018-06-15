956ceaf3fa121c4b64b75c2fd8c9abaf1ea973f6
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-06-15 15:59:31 +0100

    Cleaned up code

--- struggle_input/urls.py
+++ struggle_input/urls.py
@@ -5,47 +5,6 @@ from .views import AddStruggle
 from . import views
 
 urlpatterns = [
-	# url(r'^search/$', views.search),
-	# url(r'^struggles/$', views.add_struggle),
-	# url(r'^search/$', views.search),
 	url(r'^struggles/input$', AddStruggle.as_view(), name='struggles_input'),
 	url(r'^struggles/search$', views.search, name='struggles_search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-# ###urls.py###
-# url(r'^$', views.landing.as_view(), name = 'landing'),
-
-# ####views.py####
-# class landing(View):
-#     template_name = '/home.html'
-#     form_class1 = forms.pynamehere1
-#     form_class2 = forms.pynamehere2
-#         def get(self, request):
-#             form1 = self.form_class1(None)
-#             form2 = self.form_class2(None)
-#             return render(request, self.template_name, { 'register':form1, 'login':form2,})
-
-#          def post(self, request):
-#              if request.method=='POST' and 'htmlsubmitbutton1' in request.POST:
-#                     ## do what ever you want to do for first function ####
-#              if request.method=='POST' and 'htmlsubmitbutton2' in request.POST:
-#                      ## do what ever you want to do for second function ####
-#                     ## return def post###  
-#              return render(request, self.template_name, {'form':form,})
-
-
-# ####/home.html####
-# #### form 1 ####
-# <form action="" method="POST" >
-#   {% csrf_token %}
-#   {{ register.as_p }}
-# <button type="submit" name="htmlsubmitbutton1">Login</button>
-# </form>
-# #### form 2 ####
-# <form action="" method="POST" >
-#   {% csrf_token %}
-#   {{ login.as_p }}
-# <button type="submit" name="htmlsubmitbutton2">Login</button>
-# </form>
--- struggle_input/views.py
+++ struggle_input/views.py
@@ -25,51 +25,10 @@ def search(request):
 	return render(request, 'struggle_input/struggle_form.html', {'errors': errors, 'form': form, 'searching': True})
 
 
-# def add_struggle(request):
-#     if request.method == 'POST':
-#         form = StruggleModelForm(request.POST, request.FILES)
-#         print(form)
-#         if form.is_valid():
-#             date = form.cleaned_data['date']
-#             title = form.cleaned_data['title']
-#             struggle = form.cleaned_data['struggle']
-#             plan = form.cleaned_data['plan']
-#             frustration_level = form.cleaned_data['frustration_level']
-#             time_off_task = form.cleaned_data['time_off_task']
-#             code_screen_shot = form.cleaned_data['code_screen_shot']
-      
-#             struggle_data = StruggleData.objects.create(
-#             									 title=title,
-#             									 date=date,
-#             									 struggle=struggle, 
-#             									 plan=plan, 
-#             									 frustration_level=frustration_level,
-#             									 time_off_task=time_off_task,
-#             									 code_screen_shot= request.FILES['code_screen_shot']
-#                                          		)
-#             return HttpResponseRedirect('/struggles')
-#     else:
-#         form = StruggleModelForm()
-
-#     return render(request, 'struggle_input/struggle_form.html', {'form': form})
-
-
 class AddStruggle(FormView):
     form_class = StruggleModelForm
     template_name = 'struggle_input/struggle_form.html'
     success_url = reverse_lazy('struggles_input')
-
-    # def get(self, request, *args, **kwargs):
-    #     form = self.form_class(initial=self.initial)
-    #     return render(request, self.template_name, {'form': form})
-
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-    #     if form.is_valid():
-    #         # <process form cleaned data>
-    #         return HttpResponseRedirect('/struggles/')
-
-        # return render(request, self.template_name, {'form': form})
 
     def form_valid(self, form):
     	form.save()
