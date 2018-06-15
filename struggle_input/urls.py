40d589411a894748321d1763eddc38315e9b32b6
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-06-15 17:30:58 +0100

    Create a link to show detail on problem

--- struggle_input/urls.py
+++ struggle_input/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
-from .views import AddStruggle
+from .views import AddStruggle, DetailStruggleView
+from django.urls import include, path
 from . import views
 
+
 urlpatterns = [
+	path('<pk>/', DetailStruggleView.as_view(), name='struggle-detail'),
 	url(r'^struggles/input$', AddStruggle.as_view(), name='struggles_input'),
 	url(r'^struggles/search$', views.search, name='struggles_search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- struggle_input/views.py
+++ struggle_input/views.py
@@ -8,21 +8,24 @@ import datetime
 from django.urls import reverse
 from django.template import RequestContext
 from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
+from django.utils import timezone
+
 
 
 def search(request):
-	form = StruggleModelForm()
-	errors = []
-	if 'q' in request.GET:
-		q = request.GET['q']
-		if not q:
-			errors.append('Enter a search term.')
-		elif len(q) > 20:
-			errors.append('Please enter at most 20 characters.')
-		else:
-			struggles = StruggleData.objects.filter(title__icontains=q)
-			return render(request, 'struggle_input/struggle_form.html',{'struggles': struggles, 'query': q, 'form': form, 'searching': True})
-	return render(request, 'struggle_input/struggle_form.html', {'errors': errors, 'form': form, 'searching': True})
+    form = StruggleModelForm()
+    errors = []
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            struggles = StruggleData.objects.filter(title__icontains=q)
+            return render(request, 'struggle_input/struggle_form.html',{'struggles': struggles, 'query': q, 'form': form, 'searching': True})
+    return render(request, 'struggle_input/struggle_form.html', {'errors': errors, 'form': form, 'searching': True})
 
 
 class AddStruggle(FormView):
@@ -31,8 +34,18 @@ class AddStruggle(FormView):
     success_url = reverse_lazy('struggles_input')
 
     def form_valid(self, form):
-    	form.save()
-    	return super().form_valid(form)
+        form.save()
+        return super().form_valid(form)
+
+
+class DetailStruggleView(DetailView):
+    model = StruggleData
+    template_name = 'struggle_input/struggle-detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
 
 
 
